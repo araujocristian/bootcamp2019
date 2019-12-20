7cbd32f29bf291641546f22a186139c021bb4ae9
Cristian Araujo <araujocristian@live.com>
2019-12-19 21:24:07 -0300

    add authentication and user update

--- desafios/gympoint/src/routes.js
+++ desafios/gympoint/src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middleware/auth';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
@@ -9,6 +11,8 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
 
