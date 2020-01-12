45863eb77fe82ac7c4407fd26388f87f7c80e50f
Cristian Araujo <araujocristian@live.com>
2020-01-12 14:25:49 -0300

    Criado Agendamento de Serviço

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -5,9 +5,10 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileContoller';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
-import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,6 +21,9 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
