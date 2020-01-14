b8660bdb9e7d56794e8cc6c576e9d45557eaef57
Cristian Araujo <araujocristian@live.com>
2020-01-14 20:21:42 -0300

    Configurando Nodemailer

---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,24 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'bbf4db9a365976',
+    pass: '79e0b97ebb4202',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Servidores de email
+ *
+ * Amazon SES
+ * Mailgun
+ * SparkPost
+ * Mandril (Mailchimp)
+ * Não usar o do GMAIL
+ *
+ * Mailtrap (DEV)
+ */
