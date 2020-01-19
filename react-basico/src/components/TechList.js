fe78f5d6faeaaf427ef037a6f29d3e4aa17bf246
Cristian Araujo <araujocristian@live.com>
2020-01-18 23:06:41 -0300

    React 1 - Ciclo de vida do componente

--- react-basico/src/components/TechList.js
+++ react-basico/src/components/TechList.js
@@ -4,8 +4,24 @@ import TechItem from "./TechItem";
 export class TechList extends Component {
   state = {
     newTech: "",
-    techs: ["Node.js", "ReactJS", "React Native"]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estados
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
