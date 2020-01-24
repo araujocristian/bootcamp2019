5199b8cbe5195a2e338832492659642c423d31ff
Cristian Araujo <araujocristian@live.com>
2020-01-24 19:33:52 -0300

    Carregando dados da API

--- list-repo/src/pages/Repository/index.js
+++ list-repo/src/pages/Repository/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match: { params } }) {
-  return <div>Repo: {decodeURIComponent(params.repository)}</div>;
+export default class Repository extends Component {
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const {
+      match: { params },
+    } = this.props;
+
+    const repoName = decodeURIComponent(params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+    return <div>Repo: {decodeURIComponent(params.repository)}</div>;
+  }
 }
